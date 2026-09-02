ad7fe9f2d71d649295060fc667f172d34c2084c6
Daouda Mare <daouda.mare@yametba.com>
2026-09-02 21:27:21 +0000

    deploy7

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ php artisan migrate --force
 php artisan storage:link
 php artisan filament:assets
 php artisan db:seed --force
+php artisan relay:register "Samsung A12" --network=orange
 php-fpm -D
 nginx -g "daemon off;"
